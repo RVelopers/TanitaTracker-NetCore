83de16803b621b709a57e7af28e1b26a6ae4816a
William Bello <willjobs94@gmail.com>
2018-05-13 00:37:07 -0400

    Add initial project structure

---
+++ TanitaTrakerNetCore/Web-NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
