c4bc195541c03da745f69f3b5e7f7069ae678988
Christian Rodriguez <admin@Christian-Rodriguez.local>
2018-05-15 23:10:25 -0400

    Added Template Inspinia

--- TanitaTrakerNetCore/Web-NetCore/Controllers/HomeController.cs
+++ TanitaTrakerNetCore/Web-NetCore/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace Web_NetCore.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["SubTitle"] = "Welcome to Tanita Tracker";
+            ViewData["Message"] = "It is an application to work alone with the Omron Body Composition Monitor and Scale (HBF-514C) that allows you to register user, add multiple body scan to each individual user and create reports based those data points.";
             return View();
         }
 
