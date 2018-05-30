a8c25b80ad0487bb88028af8001b5dc8690f9a81
christianrd <me@christiandr.com>
2018-05-29 22:01:16 -0400

    Created Roles and User Roles #22

---
+++ TanitaTrackerNetCore/Domain/Entities/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities
+{
+    public class Roles : SoftDeletableEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }    
+    }
+}
---
+++ TanitaTrackerNetCore/Domain/Entities/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities
+{
+    public class UserRoles : BaseEntity
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
