f966159e6f4228ea2c4f2e98d21d18ad1da1be0d
christianrd <christiandr17@icloud.com>
2018-05-31 22:41:08 -0400

    Added Roles migration

---
+++ TanitaTrackerNetCore/DatabaseMigration/Migrations/20180531201800_create_roles_table.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseMigration.Framework;
+using Domain.Entities;
+using FluentMigrator;
+
+namespace DatabaseMigration.Migrations
+{
+    [Migration(20180531201800)]
+    public class _20180531201800_create_roles_table : SoftDeleteEntityMigration<Roles>
+    {
+        public override void Up()
+        {
+            base.Up();
+            CreateColumnOnTable(nameof(Entity.Name)).AsAnsiString().Nullable();
+            CreateColumnOnTable(nameof(Entity.Description)).AsAnsiString().Nullable();
+        }
+    }
+}
---
+++ TanitaTrackerNetCore/DatabaseMigration/Migrations/20180531203000_create_users_roles_table.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseMigration.Framework;
+using Domain.Entities;
+using FluentMigrator;
+
+namespace DatabaseMigration.Migrations
+{    
+    [Migration(20180531203000)]
+    public class _20180531203000_create_users_roles_table : EntityMigration<UserRoles>
+    {
+        public override void Up()
+        {
+            base.Up();
+            CreateForeignKeyColumn<User>(nameof(Entity.UserId));
+            CreateForeignKeyColumn<Roles>(nameof(Entity.RoleId));
+        }
+    }
+}
