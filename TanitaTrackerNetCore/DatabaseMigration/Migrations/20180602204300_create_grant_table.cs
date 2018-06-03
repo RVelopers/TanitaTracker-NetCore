2fc68d3af21f5d5346a86f52a9796e9a0e3b2795
christianrd <christiandr17@icloud.com>
2018-06-02 20:59:36 -0400

    Created Grant migration

---
+++ TanitaTrackerNetCore/DatabaseMigration/Migrations/20180602204300_create_grant_table.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseMigration.Framework;
+using Domain.Entities;
+using FluentMigrator;
+
+namespace DatabaseMigration.Migrations
+{
+    [Migration(20180602204300)]    
+    public class _20180602204300_create_grant_table : SoftDeleteEntityMigration<Grant>
+    {
+        public override void Up()
+        {
+            base.Up();
+            CreateForeignKeyColumn<Roles>(nameof(Entity.RoleId));
+            CreateForeignKeyColumn<Module>(nameof(Entity.ModuleId));
+            CreateColumnOnTable(nameof(Entity.Method)).AsAnsiString().Nullable();
+
+        }
+    }
+}
