b409dee9995a8785d4f449721ec61cdb5f722076
christianrd <christiandr17@icloud.com>
2018-08-19 09:45:25 -0400

    f

---
+++ TanitaTrackerNetCore/Web-NetCore/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_NetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET
+        public IActionResult Login()
+        {
+            ViewData["Title"] = "Login";
+            return View();
+        }
+        
+        // GET
+        public IActionResult Logout()
+        {
+            return Redirect("Login");
+        }
+    }
+}
